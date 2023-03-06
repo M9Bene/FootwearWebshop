ca396d9c79bee89d6dd1087d44b158fc44914eca
M9Bene <bene9marci@gmail.com>
2023-03-06 15:32:52 +0100

    Add: Shoe class with basic properties

---
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/model/Shoe.java
@@ -0,0 +1,42 @@
+package com.flashforward.backendspring.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+@Entity
+public class Shoe {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Column(name = "brand", nullable = false)
+    private String brand;
+
+    @Column(name = "price", nullable = false)
+    private double price;
+
+
+    public String getBrand() {
+        return brand;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+}
