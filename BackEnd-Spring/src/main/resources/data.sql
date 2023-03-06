ec8b74e969b0573f666c16b81ff9b5633cfd1a3c
M9Bene <bene9marci@gmail.com>
2023-03-06 15:47:51 +0100

    Add: data initialization

---
+++ BackEnd-Spring/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO shoe (name, brand, price) VALUES ('Air Jordan 1 Mid Knicks', 'nike', 119.99);
+INSERT INTO shoe (name, brand, price) VALUES ('Air Jordan 1 Retro High OG Gorge Green', 'nike', 109.99);
+INSERT INTO shoe (name, brand, price) VALUES ('Yeezy Boost 350 V2 Jade Ash', 'adidas', 129.99);
+INSERT INTO shoe (name, brand, price) VALUES ('Yeezy Boost 350 V2 Zebra', 'adidas', 129.99);
+INSERT INTO shoe (name, brand, price) VALUES ('Air Jordan 6 Retro Travis Scott', 'nike', 89.99);
