593368223bb5d7c4ccd2c6c99fbdf4509d192d18
M9Bene <bene9marci@gmail.com>
2023-03-07 13:20:29 +0100

    Add: BasicShoeInfoDTO converter

--- BackEnd-Spring/src/main/java/com/flashforward/backendspring/service/ShoeService.java
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/service/ShoeService.java
@@ -1,6 +1,8 @@
 package com.flashforward.backendspring.service;
 
 
+import com.flashforward.backendspring.dto.BasicShoeInfoDTO;
+import com.flashforward.backendspring.model.Shoe;
 import com.flashforward.backendspring.repository.ShoeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -17,4 +19,10 @@ public class ShoeService {
     }
 
 
+
+    // CONVERTS SHOE CLASS TO BASIC SHOE INFO DTO CLASS
+    private BasicShoeInfoDTO basicShoeInfoDTOConverter(Shoe shoe) {
+
+        return new BasicShoeInfoDTO(shoe.getId(), shoe.getName(), shoe.getBrand(), shoe.getPrice());
+    }
 }
