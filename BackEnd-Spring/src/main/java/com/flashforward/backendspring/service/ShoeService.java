7485d1f9fab288df98d9d154e7b14b8febf65504
M9Bene <bene9marci@gmail.com>
2023-03-07 13:25:09 +0100

    Add: method to return all shoes as basicShoeInfoDTO

--- BackEnd-Spring/src/main/java/com/flashforward/backendspring/service/ShoeService.java
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/service/ShoeService.java
@@ -1,12 +1,13 @@
 package com.flashforward.backendspring.service;
 
-
 import com.flashforward.backendspring.dto.BasicShoeInfoDTO;
 import com.flashforward.backendspring.model.Shoe;
 import com.flashforward.backendspring.repository.ShoeRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class ShoeService {
@@ -18,6 +19,15 @@ public class ShoeService {
         this.shoeRepo = shoeRepo;
     }
 
+    // RETURNS ALL SHOES FROM REPO CONVERTED TO BASIC SHOE INFO DTO CLASS
+    public List<BasicShoeInfoDTO> getAllBasicShoeInfo() {
+
+        List<Shoe> allShoes = shoeRepo.findAll();
+
+        return allShoes.stream()
+                .map(this::basicShoeInfoDTOConverter)
+                .collect(Collectors.toList());
+    }
 
 
     // CONVERTS SHOE CLASS TO BASIC SHOE INFO DTO CLASS
