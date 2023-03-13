c1ffb0c0a253b9f3d6221fe8539255794b5e4331
M9Bene <bene9marci@gmail.com>
2023-03-13 16:31:37 +0100

    Add: method to return shoes by brand and price order and in between price range + deleting unnecessary methods

--- BackEnd-Spring/src/main/java/com/flashforward/backendspring/repository/ShoeRepo.java
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/repository/ShoeRepo.java
@@ -9,21 +9,12 @@ import java.util.List;
 
 public interface ShoeRepo extends JpaRepository <Shoe, Integer>{
 
-        List<Shoe> findAll();
-
-        List<Shoe> findAll(Sort sort);
-
-        List<Shoe> findAllByBrand(String brand);
-
-        List<Shoe> findAllByBrand(String brand, Sort sort);
-
-        @Query("SELECT s FROM Shoe s WHERE s.price BETWEEN :minPrice AND :maxPrice")
-        List<Shoe> findAllWithinPriceRange(double minPrice, double maxPrice);
-
         @Query("SELECT s FROM Shoe s WHERE s.price BETWEEN :minPrice AND :maxPrice")
         List<Shoe> findAllWithinPriceRange(double minPrice, double maxPrice, Sort sort);
 
         List<Shoe> findAllByBrandAndPriceIsBetween(String brand, double minPrice, double maxPrice);
 
         List<Shoe> findAllByPriceIsBetween(double minPrice, double maxPrice);
+
+        List<Shoe> findAllByBrandAndPriceIsBetween(String brand, double minPrice, double maxPrice, Sort sort);
 }
