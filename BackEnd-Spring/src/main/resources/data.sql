9147edd7ee4da7a22eabd65d2be2f55967c9eaa5
M9Bene <bene9marci@gmail.com>
2023-03-16 16:52:28 +0100

    Add: placeholder image urls for init shoe data

--- BackEnd-Spring/src/main/resources/data.sql
+++ BackEnd-Spring/src/main/resources/data.sql
@@ -1,8 +1,8 @@
-INSERT INTO shoe (name, brand, price) VALUES ('Air Jordan 1 Mid Knicks', 'nike', 119.99);
-INSERT INTO shoe (name, brand, price) VALUES ('Air Jordan 1 Retro High OG Gorge Green', 'nike', 109.99);
-INSERT INTO shoe (name, brand, price) VALUES ('Yeezy Boost 350 V2 Jade Ash', 'adidas', 129.99);
-INSERT INTO shoe (name, brand, price) VALUES ('Yeezy Boost 350 V2 Zebra', 'adidas', 129.99);
-INSERT INTO shoe (name, brand, price) VALUES ('Air Jordan 6 Retro Travis Scott', 'nike', 89.99);
+INSERT INTO shoe (name, brand, price, img_url) VALUES ('Air Jordan 1 Mid Knicks', 'nike', 119.99, 'url1');
+INSERT INTO shoe (name, brand, price, img_url) VALUES ('Air Jordan 1 Retro High OG Gorge Green', 'nike', 109.99, 'url2');
+INSERT INTO shoe (name, brand, price, img_url) VALUES ('Yeezy Boost 350 V2 Jade Ash', 'adidas', 129.99, 'url3');
+INSERT INTO shoe (name, brand, price, img_url) VALUES ('Yeezy Boost 350 V2 Zebra', 'adidas', 129.99, 'url4');
+INSERT INTO shoe (name, brand, price, img_url) VALUES ('Air Jordan 6 Retro Travis Scott', 'nike', 89.99, 'url5');
 
 INSERT INTO size_and_quantity (size, quantity, shoe_id) VALUES (40, 5, 1);
 INSERT INTO size_and_quantity (size, quantity, shoe_id) VALUES (41, 3, 1);
