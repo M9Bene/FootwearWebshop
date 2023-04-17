a1580f2fde775e0c938fe40ca23d1a35fe8f5a8d
M9Bene <bene9marci@gmail.com>
2023-04-17 14:05:56 +0200

    Add: html for menu-drop-down

--- frontend-react/src/components/Navbar.js
+++ frontend-react/src/components/Navbar.js
@@ -1,23 +1,35 @@
 import './Navbar.css';
 
 
+
 function Navbar() {
 
-
     return (
-        <div className="navbar">
-            <div className={"logo"}>MARCI-STORE</div>
-            <ul className={"menu-items"}>
-                <li>HOME</li>
-                <li>BRANDS</li>
-                <li>ABOUT</li>
-                <li>HELP</li>
-            </ul>
-            <div className={"active-icon-container"}>
-                <div className={"active-icon"}>menu</div>
-                <div className={"active-icon"}>login</div>
+        <div className={"header"}>
+            <div className="navbar">
+                <div className={"logo"}>MARCI-STORE</div>
+                <ul className={"menu-items"}>
+                    <li>HOME</li>
+                    <li>BRANDS</li>
+                    <li>ABOUT</li>
+                    <li>HELP</li>
+                </ul>
+                <div className={"active-icon-container"}>
+                    <div className={"active-icon"}>cart</div>
+                    <div className={"active-icon"}>login</div>
+                </div>
+                <div className={"menu-btn"}>menu
+                </div>
+
             </div>
-            <div className={"menu-btn"}></div>
+            <div className={"drop-down-menu"}>
+                <ul className={"drop-down-menu-items"}>
+                    <li>HOME</li>
+                    <li>BRANDS</li>
+                    <li>ABOUT</li>
+                    <li>HELP</li>
+                </ul>
+            </div>
         </div>
     );
 }
