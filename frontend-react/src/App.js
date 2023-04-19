69ee32b9af59639d59ab741bc03677c766e6f810
M9Bene <bene9marci@gmail.com>
2023-04-19 19:30:53 +0200

    Add: Footer to App js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,14 +1,20 @@
 import './App.css';
 
 import Navbar from "./components/Navbar";
+import Footer from "./components/Footer";
 
 
 function App() {
-  return (
-    <div className="App">
-        <Navbar></Navbar>
-    </div>
-  );
+    return (
+        <div className="App">
+            <div className={"body"}>
+                <Navbar/>
+                <div> something </div>
+                <Footer/>
+            </div>
+
+        </div>
+    );
 }
 
 export default App;
