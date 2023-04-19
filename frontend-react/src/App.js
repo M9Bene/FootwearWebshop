dbda8b043edb133a0fba3b033063cb4bf01fa9b9
M9Bene <bene9marci@gmail.com>
2023-04-19 19:50:32 +0200

    Remove: unnecessary div

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
@@ -7,12 +6,8 @@ import Footer from "./components/Footer";
 function App() {
     return (
         <div className="App">
-            <div className={"body"}>
                 <Navbar/>
-                <div> something </div>
                 <Footer/>
-            </div>
-
         </div>
     );
 }
