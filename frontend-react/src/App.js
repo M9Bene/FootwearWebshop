cc70feb58511541c24388c274b24ecaa341af85f
M9Bene <bene9marci@gmail.com>
2023-04-20 17:22:00 +0200

    Add: card container + ShoeCard components

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ShoeCard from "./components/ShoeCard";
 
 
 function App() {
+
     return (
         <div className="App">
-                <Navbar/>
-                <Footer/>
+            <Navbar/>
+            <div className={"card-container"}>
+                <ShoeCard />
+                <ShoeCard />
+                <ShoeCard />
+                <ShoeCard />
+                <ShoeCard />
+            </div>
+            <Footer/>
         </div>
     );
 }
