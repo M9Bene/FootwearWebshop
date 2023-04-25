d2c86e7223978938b240e3e8bc217a025bf039e0
M9Bene <bene9marci@gmail.com>
2023-04-25 13:20:20 +0200

    Add: isRangeOptions condition for Filter component to display its option as a multi range slider using that component

--- frontend-react/src/components/Filter.js
+++ frontend-react/src/components/Filter.js
@@ -1,26 +1,42 @@
 import {useState} from "react";
 import './Filter.css';
+import MultiRangeSlider from "./MultiRangeSlider";
 
-
-function Filter({title, options, setFilter}) {
+function Filter({title, options, setFilter, isRangeOption}) {
 
     const [showOptions, setShowOptions] = useState(false);
     const [selected, setSelected] = useState("all");
 
+    if (isRangeOption) {
+        // render Filter Component with a Range Slider as 'options'
+        return (
+            <div className={"filter"}>
+                <div className={"filter-title"} onClick={() => setShowOptions(!showOptions)}>{title} Selection</div>
 
-    return (<div className={"filter"}>
-        <div className={"filter-title"} onClick={() => setShowOptions(!showOptions)}>{title} Selection</div>
+                <div className={!showOptions && "hide"}>
+                    <MultiRangeSlider min={0.0} max={400.0}
+                                      onChange={({min, max}) => {setFilter(title, min, max)}}
+                    />
+                </div>
+            </div>
+        )
 
-        {options.map((option, i) => {
-            return (<div key={i}
-                         className={showOptions && selected === option ? "selected filter-option"
-                             : showOptions ? "filter-option" : "hide"}
-                         onClick={() => {
-                             setFilter(title, option);
-                             setSelected(option);
-                         }}>{option}</div>)
-        })}
-    </div>)
+    } else {
+        // render Filter Component with divs as 'options'
+        return (<div className={"filter"}>
+            <div className={"filter-title"} onClick={() => setShowOptions(!showOptions)}>{title} Selection</div>
+
+            {options.map((option, i) => {
+                return (<div key={i}
+                             className={showOptions && selected === option ? "selected filter-option"
+                                 : showOptions ? "filter-option" : "hide"}
+                             onClick={() => {
+                                 setFilter(title, option);
+                                 setSelected(option);
+                             }}>{option}</div>)
+            })}
+        </div>)
+    }
 }
 
 export default Filter;
