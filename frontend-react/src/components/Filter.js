1245b8bc7cca8ea848d6773e2b56896e5c9524c8
M9Bene <bene9marci@gmail.com>
2023-04-27 10:47:17 +0200

    Add: usage of new Slider Component in Filter component without 'selected' useState

--- frontend-react/src/components/Filter.js
+++ frontend-react/src/components/Filter.js
@@ -1,22 +1,20 @@
 import {useState} from "react";
 import './Filter.css';
-import MultiRangeSlider from "./MultiRangeSlider";
+import DoubleSlider from "./DoubleSlider";
 
-function Filter({title, options, setFilter, isRangeOption}) {
+
+function Filter({title, options, setFilter, isRangeOption, selectedOption, selectedMinValue, selectedMaxValue}) {
 
     const [showOptions, setShowOptions] = useState(false);
-    const [selected, setSelected] = useState("all");
 
     if (isRangeOption) {
         // render Filter Component with a Range Slider as 'options'
         return (
             <div className={"filter"}>
-                <div className={"filter-title"} onClick={() => setShowOptions(!showOptions)}>{title} Selection</div>
-
-                <div className={!showOptions && "hide"}>
-                    <MultiRangeSlider min={0.0} max={400.0}
-                                      onChange={({min, max}) => {setFilter(title, min, max)}}
-                    />
+                <div className={"filter-title"} onClick={() => setShowOptions(!showOptions)}>{title} selection</div>
+                <div className={!showOptions ? "hide" : ""}>
+                    <DoubleSlider min={0.0} max={400.0}  onChange={({min, max}) => {setFilter(title, min, max)}}
+                                  selectedMinValue={selectedMinValue} selectedMaxValue={selectedMaxValue}/>
                 </div>
             </div>
         )
@@ -24,15 +22,14 @@ function Filter({title, options, setFilter, isRangeOption}) {
     } else {
         // render Filter Component with divs as 'options'
         return (<div className={"filter"}>
-            <div className={"filter-title"} onClick={() => setShowOptions(!showOptions)}>{title} Selection</div>
+            <div className={"filter-title"} onClick={() => setShowOptions(!showOptions)}>{title} selection</div>
 
             {options.map((option, i) => {
                 return (<div key={i}
-                             className={showOptions && selected === option ? "selected filter-option"
+                             className={showOptions && selectedOption === option ? "selected filter-option"
                                  : showOptions ? "filter-option" : "hide"}
                              onClick={() => {
                                  setFilter(title, option);
-                                 setSelected(option);
                              }}>{option}</div>)
             })}
         </div>)
