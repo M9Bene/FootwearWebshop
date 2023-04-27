8c09b25b182e353c0f7437a6fc7cb5446fc87238
M9Bene <bene9marci@gmail.com>
2023-04-27 11:05:51 +0200

    Add: usage of FilterAndCards component in App.js

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,21 +1,17 @@
 import './App.css';
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
-import ShoeCard from "./components/ShoeCard";
+import FilterAndCards from "./components/FilterAndCards";
 
 
 function App() {
 
+
+
     return (
         <div className="App">
             <Navbar/>
-            <div className={"card-container"}>
-                <ShoeCard />
-                <ShoeCard />
-                <ShoeCard />
-                <ShoeCard />
-                <ShoeCard />
-            </div>
+            <FilterAndCards/>
             <Footer/>
         </div>
     );
