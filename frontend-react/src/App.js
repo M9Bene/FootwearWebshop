9bf4915cdbd7ec0229488f8c96f52cae8cd06ab0
M9Bene <bene9marci@gmail.com>
2023-05-02 19:20:52 +0200

    Add: CartContext as wrapper for ContentHolder

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -3,7 +3,7 @@ import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import ContentHolder from "./components/ContentHolder";
 import {memo, useState} from "react";
-
+import {CartContextProvider} from "./cart_storage/CartContext";
 
 function App() {
 
@@ -13,7 +13,13 @@ function App() {
     return (
         <div className="App">
             <MemoizedNavbar setContent={setContent}/>
+
+            <CartContextProvider>
+
             <ContentHolder content={content} setContent={setContent}/>
+
+            </CartContextProvider>
+
             <MemoizedFooter/>
         </div>
     );
