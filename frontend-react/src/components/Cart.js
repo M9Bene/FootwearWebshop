644594b736013fa19c460e241722bdc8ca510c83
M9Bene <bene9marci@gmail.com>
2023-05-03 15:56:52 +0200

    Add: total price for cart

--- frontend-react/src/components/Cart.js
+++ frontend-react/src/components/Cart.js
@@ -9,17 +9,31 @@ function Cart() {
     const {getAllCartItems} = useContext(CartContext);
 
     const [cartItems, setCartItems] = useState(getAllCartItems)
+    const [totalPrice, setTotalPrice] = useState(0)
+
+    
 
     useEffect(
         () => {
+            // function for count total Price according to products price and quantity
+            const handleTotalPrice = () => {
+                let totalPrice = 0;
+                for (const cartItem of cartItems) {
+                    totalPrice += cartItem.price * cartItem.quantity;
+                }
+                setTotalPrice(totalPrice);
+            }
+            // set updated cart items
             setCartItems(getAllCartItems);
+            //  count total Price
+            handleTotalPrice();
         }
-    , [getAllCartItems]);
+    , [cartItems, getAllCartItems]);
 
     return (
         <div className={"cart-container"}>
             <div className={"cart-title"}>Your Bag!</div>
-
+            <div className={"cart-cost"}>Total cost: {totalPrice.toFixed(2)}</div>
             <div className={"cart-item-container"}>
                 {cartItems.map((product, i) => (
                     <CartItem key={i} product={product}/>
