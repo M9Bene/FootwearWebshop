d32301875fb4912a0be0451cf17d848499c9c17f
M9Bene <bene9marci@gmail.com>
2023-05-11 16:30:40 +0200

    Add: User class with  basic attributes and annotations

---
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/model/User.java
@@ -0,0 +1,34 @@
+package com.flashforward.backendspring.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Table(name = "_user")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    private int id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String password;
+}
