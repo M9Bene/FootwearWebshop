9a7ac2ce3209570ba9e3e5a464da52f2a1b77163
M9Bene <bene9marci@gmail.com>
2023-05-12 15:08:02 +0200

    Add: Role attribute for User class + usage of this field in  getAuthorities method

--- BackEnd-Spring/src/main/java/com/flashforward/backendspring/model/User.java
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/model/User.java
@@ -1,17 +1,17 @@
 package com.flashforward.backendspring.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import com.flashforward.backendspring.security.Role;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
+import java.util.List;
 
 import static jakarta.persistence.GenerationType.IDENTITY;
 
@@ -36,9 +36,12 @@ public class User implements UserDetails {
 
     private String password;
 
+    @Enumerated(EnumType.STRING)
+    private Role role;
+
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return null;
+        return List.of(new SimpleGrantedAuthority(role.name()));
     }
 
     @Override
