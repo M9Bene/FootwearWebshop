30d893247b7234daa8da97dd478554c9f68c2b77
M9Bene <bene9marci@gmail.com>
2023-05-14 13:10:17 +0200

    Add: method to convert secret_key from String to Key

--- BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/JWTService.java
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/JWTService.java
@@ -1,6 +1,10 @@
 package com.flashforward.backendspring.security;
 
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
 
 @Service
 public class JWTService {
@@ -8,6 +12,10 @@ public class JWTService {
     //  a 256bit Encryption key in HEX for: sign in key
     private static final String SECRET_KEY = "66546A576E5A7234753778214125442A462D4A614E645267556B587032733576";
 
-
+    // convert the SECRET_KEY String into bytes then (convert into and ) return it as Key
+    private Key getSignInKey(){
+        byte [] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(keyBytes);
+    }
 
 }
