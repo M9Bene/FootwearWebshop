50faa5f7646852f9c22ea144186b74230d7dfbc6
M9Bene <bene9marci@gmail.com>
2023-05-14 12:56:07 +0200

    Add: JWT Service class with Secret Key

---
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/JWTService.java
@@ -0,0 +1,13 @@
+package com.flashforward.backendspring.security;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class JWTService {
+
+    //  a 256bit Encryption key in HEX for: sign in key
+    private static final String SECRET_KEY = "66546A576E5A7234753778214125442A462D4A614E645267556B587032733576";
+
+
+
+}
