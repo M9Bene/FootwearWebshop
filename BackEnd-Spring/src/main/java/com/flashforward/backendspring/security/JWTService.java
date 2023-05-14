7e9afa72743c7c7cd59b2e2fc8a2e551aeba9762
M9Bene <bene9marci@gmail.com>
2023-05-14 13:53:26 +0200

    Add: method to retrieve all the claims from token

--- BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/JWTService.java
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/JWTService.java
@@ -1,5 +1,7 @@
 package com.flashforward.backendspring.security;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.stereotype.Service;
@@ -12,10 +14,19 @@ public class JWTService {
     //  a 256bit Encryption key in HEX for: sign in key
     private static final String SECRET_KEY = "66546A576E5A7234753778214125442A462D4A614E645267556B587032733576";
 
+    // get all the claims from token
+    private Claims extractClaims(String token){
+        return Jwts
+                .parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
     // convert the SECRET_KEY String into bytes then (convert into and ) return it as Key
     private Key getSignInKey(){
         byte [] keyBytes = Decoders.BASE64.decode(SECRET_KEY);
         return Keys.hmacShaKeyFor(keyBytes);
     }
-
 }
