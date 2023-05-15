95c3f66d0ade592119d2ec5badea00f21cc1a3da
M9Bene <bene9marci@gmail.com>
2023-05-15 12:57:16 +0200

    Add: password encoder (using BCrypt) Bean

--- BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/ApplicationConfig.java
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/ApplicationConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @RequiredArgsConstructor
@@ -14,8 +16,13 @@ public class ApplicationConfig {
     private final UserRepo userRepo;
 
     @Bean
-    public UserDetailsService userDetailsService(){
+    public UserDetailsService userDetailsService() {
         return username -> userRepo.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
