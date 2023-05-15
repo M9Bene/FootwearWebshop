ab889b3bfd39c789b0699443cf130486d6bd0cc3
M9Bene <bene9marci@gmail.com>
2023-05-15 12:58:03 +0200

    Add: authentication provider Bean

--- BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/ApplicationConfig.java
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/ApplicationConfig.java
@@ -4,6 +4,8 @@ import com.flashforward.backendspring.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -19,6 +21,14 @@ public class ApplicationConfig {
     public UserDetailsService userDetailsService() {
         return username -> userRepo.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("user not found"));
+    }
+
+    @Bean
+    public AuthenticationProvider authenticationProvider() {
+        DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+        authProvider.setUserDetailsService(userDetailsService());
+        authProvider.setPasswordEncoder(passwordEncoder());
+        return authProvider;
     }
 
     @Bean
