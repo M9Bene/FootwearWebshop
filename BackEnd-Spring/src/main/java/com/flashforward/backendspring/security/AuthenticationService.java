5e7478f362764b23d13f566bfab9606fc5190add
M9Bene <bene9marci@gmail.com>
2023-05-16 13:02:59 +0200

    Add: authenticate method in AuthenticationService

--- BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/AuthenticationService.java
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/security/AuthenticationService.java
@@ -4,6 +4,8 @@ package com.flashforward.backendspring.security;
 import com.flashforward.backendspring.model.User;
 import com.flashforward.backendspring.repository.UserRepo;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
@@ -14,6 +16,7 @@ public class AuthenticationService {
     private final UserRepo userRepo;
     private final PasswordEncoder passwordEncoder;
     private final JWTService jwtService;
+    private final AuthenticationManager authManager;
 
     public AuthenticationResponse register(RegisterRequest request) {
         User user = User.builder()
@@ -28,14 +31,25 @@ public class AuthenticationService {
 
         String jwtToken = jwtService.generateToken(user);
 
-        return  AuthenticationResponse.builder()
+        return AuthenticationResponse.builder()
                 .token(jwtToken)
                 .build();
     }
 
     public AuthenticationResponse authenticate(RegisterRequest request) {
 
-        return null;
+        authManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()));
 
+        User user = userRepo.findByEmail(request.getEmail())
+                .orElseThrow();
+
+        String jwtToken = jwtService.generateToken(user);
+
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
     }
 }
