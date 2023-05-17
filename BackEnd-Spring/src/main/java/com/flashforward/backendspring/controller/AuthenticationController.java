9e8a7693f0c43d727786952c114aa8bc90982013
M9Bene <bene9marci@gmail.com>
2023-05-17 15:09:33 +0200

    Add: Authentication controller with register and authenticate endpoints

---
+++ BackEnd-Spring/src/main/java/com/flashforward/backendspring/controller/AuthenticationController.java
@@ -0,0 +1,29 @@
+package com.flashforward.backendspring.controller;
+
+import com.flashforward.backendspring.security.AuthenticationRequest;
+import com.flashforward.backendspring.security.AuthenticationResponse;
+import com.flashforward.backendspring.security.AuthenticationService;
+import com.flashforward.backendspring.security.RegisterRequest;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthenticationController {
+
+    private final AuthenticationService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<AuthenticationResponse> register(@RequestBody RegisterRequest request){
+
+        return ResponseEntity.ok(authService.register(request));
+    }
+
+
+    @PostMapping("/authenticate")
+    public ResponseEntity<AuthenticationResponse> authenticateRequest(@RequestBody AuthenticationRequest request){
+        return ResponseEntity.ok(authService.authenticate(request));
+    }
+}
